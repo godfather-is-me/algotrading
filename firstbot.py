b986f39664bf47445fae0ea31f1891fec00b2f4e
Prathyush Prashanth Rao <jay.prathyush3615@gmail.com>
2021-08-28 10:42:31 +0530

    first commit

---
+++ firstbot.py
@@ -0,0 +1,9 @@
+from fmclient import Agent, Order, OrderType, OrderSide, Holding, Market
+
+FM_ACCOUNT = "pollent-broker"
+FM_EMAIL = "prathyushr@student.unimelb.edu.au"
+FM_PASSWORD = "1102225"
+MARKETPLACE_ID = ""
+
+class MyFirstBot(Agent):
+    pass
---
+++ work2.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+print("Hello world!")
+
+# Use algovenv/Scripts/activate to activate the virtual environment in the algotrading folder
+# And `deactivate` to deactivate it
