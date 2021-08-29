96926911ebf92bfc898e7cf72f5ce35ccf38b643
Prathyush Prashanth Rao <jay.prathyush3615@gmail.com>
2021-08-30 02:25:05 +0530

    strategy comments and initialization

--- DSBot.py
+++ DSBot.py
@@ -16,6 +16,23 @@ SUBMISSION = {"number": "1102225", "name": "Prathyush Prashanth Rao"}
 # ------ Add a variable called PROFIT_MARGIN -----
 PROFIT_MARGIN = 10
 
+# Define goals - it is the equation they gave
+
+# Checks for whether there are holdlings available in the private and public market before making and buy or sell order
+# Try using the last executed price to make your strategy
+
+# The need for current orders is still vague. (One thing used is to check whether your order is in the system or not)
+    # If possible, understand the minimum selling price, maximum buying price and if opportunity exists within both execute at that point, else just place order and wait for reaction
+
+# --- learnt
+# received_holdings keeps upadating with every change in the market
+
+# Make send order function with buy/sell and price params
+
+# Use print_trade_opportunity like a switch statement and use if statements in the super method to clarify reasoning
+
+# --- Extra
+# React to changes from the manager - store the previous state and if change remove current orders in the public market
 
 # Enum for the roles of the bot
 class Role(Enum):
@@ -35,12 +52,22 @@ class DSBot(Agent):
         super().__init__(account, email, password, marketplace_id, name="PratBot")
         self._public_market_id = 0
         self._private_market_id = 0
-        self._role = None           # Buyer or seller ###
-        self._bot_type = None       # Proactive vs reactive ###
-        self._order_sent = False    # Execute only one order at a time
-        # Set intial cash, cash available, assets, assets available for both private and public
+        self._role = None               # Buyer or seller
+        self._bot_type = None           # Proactive vs reactive ###                                     ----------------- (Randomize to start off?)
+        self._pending_order = False    # Execute only one order at a time
+        # Set intial cash and widgets for both public and private markets
+        self._cash_initial = None
+        self._pub_widgets_initial = None
+        self._prv_widgets_initial = None
+        # Assets and cash - settled and available
+        self._cash_settled = None
+        self._cash_avail = None
+        self._pub_widgets_settled = None
+        self._pub_widgets_avail = None
+        self._prv_widgets_settled = None
+        self._prv_widgets_avail = None
 
-        # ------ Add new class variable _bot_type to store the type of the bot
+
 
     def role(self):
         return self._role
@@ -62,24 +89,33 @@ class DSBot(Agent):
     def order_rejected(self, info, order: Order):
         print(f"My order was rejected and the details of the order {order} are {info}")
 
+    # Function that shows various trade opportunities available
     def _print_trade_opportunity(self, other_order):
         self.inform(f"I am a {self.role()} with profitable order {other_order}")
 
-    """ Trying to send orders as a test"""
+    # Returns boolean if pending order exists
+    def _pending_order_check(orders):
+        for key, ord in Order.current().items():
+            if ord.mine and ord.is_pending:
+                return True
+        return False
+
+    # Function called with changing orders, allows us to understand the market and make informed decisions
     def received_orders(self, orders: List[Order]):
-        # Going through the list of orders
-        for ord in orders:
-            if not self._order_sent:
-                new_order = Order.create_new()
-                new_order.market = Market(self._public_market_id)
-                new_order.price = 500
-                new_order.units = 1
-                new_order.order_type = OrderType.LIMIT
-                new_order.order_side = OrderSide.SELL
-                new_order.ref = "test order"
-                self.send_order(new_order)
-                # Check to not break
-                self._order_sent = True
+        # Check if order exists in the public market
+        self._pending_order = self._pending_order_check(orders)
+
+        if not self._pending_order:
+            new_order = Order.create_new()
+            new_order.market = Market(self._public_market_id)
+            new_order.price = 500
+            new_order.units = 1
+            new_order.order_type = OrderType.LIMIT
+            new_order.order_side = OrderSide.SELL
+            new_order.ref = "test order"
+            self.send_order(new_order)
+            # Check to not break
+            self._pending_order = True
         # This is a continuous loop, use it as outermost when making decisions?
         # Check async lecture
         print("\nThese are your current orders")
@@ -97,12 +133,40 @@ class DSBot(Agent):
         print("\nThis is your last traded?")    # Yes
         print(Order.trades()[0])
 
+    def proactive():
+        pass
+
+    def reactive():
+        pass
 
     def received_holdings(self, holdings):
-        print(f"I have holding cash {holdings.cash} and cash available {holdings.cash_available}")
-        print("Also")
-        for market, asset in holdings.assets.items():
-            print(f"Assets settled {asset.units} and available {asset.units_available} for market {market}")
+
+        # Implement difference check here
+        if not (self._cash_initial == holdings.cash_initial):
+            # Check for private and public initial widgets too
+            # If check is true, restart pending orders to false
+            # Cancel all previous orders
+            pass
+
+        # Set cash
+        self._cash_initial = holdings.cash_initial
+        self._cash_avail = holdings.cash_available
+        self._cash_settled = holdings.cash
+        # Set widgets
+        for market, asset in holdings.asset.items():
+            if market.private_market:
+                self._prv_widgets_initial = asset.units_initial
+                self._prv_widgets_avail = asset.units_available
+                self._prv_widgets_settled = asset.unit
+            else:
+                self._pub_widgets_initial = asset.units_initial
+                self._pub_widgets_avail = asset.units_available
+                self._pub_widgets_settled = asset.unit
+
+        # print(f"I have holding cash {holdings.cash} and cash available {holdings.cash_available}")
+        # print("Also")
+        # for market, asset in holdings.assets.items():
+        #    print(f"Assets settled {asset.units} and available {asset.units_available} for market {market}")
 
     def received_session_info(self, session: Session):
         pass
