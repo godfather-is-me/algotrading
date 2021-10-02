7e6d881c816777c2f0f63830608b380916b74125
Prathyush Prashanth Rao <jay.prathyush3615@gmail.com>
2021-10-02 18:32:39 +0530

    constants created from excel sheet

--- CAPMBot-1.py
+++ CAPMBot-1.py
@@ -12,6 +12,9 @@ from typing import List
 from fmclient import Agent, Session
 from fmclient import Order, OrderSide, OrderType
 import numpy as np
+from itertools import combinations
+
+from numpy.lib.function_base import average
 
 # Submission details
 SUBMISSION = {"student_number": "1102225", "name": "Prathyush Prashanth Rao"}
@@ -36,6 +39,10 @@ class CAPMBot(Agent):
         """
         super().__init__(account, email, password, marketplace_id, name="CAPM Bot")
         self._payoffs = {}
+        self._payoffs_square = {}
+        self._payoffs_var = {}
+        self._payoffs_covar = {}
+
         self._risk_penalty = risk_penalty
         self._session_time = session_time
         self._market_ids = {}
@@ -43,15 +50,34 @@ class CAPMBot(Agent):
         # Initiate base values for formula
         self._cash_avail = None
         self._current_stocks = {}
+        self._current_buy = {}
+        self._current_sell = {}
 
     def initialised(self):
         # Extract payoff distribution for each security
         for _, market_info in self.markets.items():
             security = market_info.item
             description = market_info.description
-            self._payoffs[security] = [int(a) for a in description.split(",")]
+            self._payoffs[security] = np.array([int(a) for a in description.split(",")]) / 100
+
+        avg_len_div = (1 / len(self._payoffs.keys()))
+
+        # Load payoff squares
+        for key, value in self._payoffs.items():
+            self._payoffs_square[key] = np.square(value)
+
+        # Load variances
+        for key, value in self._payoffs.items():
+            self._payoffs_var[key] = (avg_len_div * np.sum(self._payoffs_square[key])) - ((avg_len_div * np.sum(value)) ** 2)
+
+        # Load covariances
+        for i in combinations(self._payoffs.keys(), 2):
+            payoff1 = self._payoffs[i[0]]
+            payoff2 = self._payoffs[i[1]]
+            self._payoffs_covar[i] = (np.dot(payoff1, payoff2) * avg_len_div) - (np.average(payoff1) * np.average(payoff2))
 
         print(self._payoffs)
+        # print(self._payoffs_covar)
         self.inform("Bot initialised, I have the payoffs for the states.")
 
     def get_potential_performance(self, orders):
@@ -62,6 +88,11 @@ class CAPMBot(Agent):
         :param orders: list of orders
         :return:
         """
+        pass
+
+    # Function to check for every purchase in current market possible
+    def _expected_payoff(self):
+        # Expected payoff given by current market prices
         pass
 
     def is_portfolio_optimal(self):
@@ -80,6 +111,25 @@ class CAPMBot(Agent):
     def received_orders(self, orders: List[Order]):
         print(Order.current().items())
 
+    # Find current minimum and maximum values for each market whenever orders is refreshed
+    def _current_market_values(self, items):
+        # refresh values to 0s
+        self._refresh_values()
+        for _, order in items:
+            if order.order_side == OrderSide.BUY:
+                self._current_buy[order.market.item] = max((order.price / 100), self._current_buy[order.market.item])
+            else:
+                self._current_sell = min((order.price / 100), self._current_sell[order.market.item])
+
+    # Refresh value with each holding
+    def _refresh_values(self):
+        for key in self._current_buy.keys():
+            self._current_buy[key] = 0
+            self._current_sell[key] = 10.0
+        # Maximum for note is 5.0
+        self._current_sell['note'] = 5.0
+
+
     def received_session_info(self, session: Session):
         pass
 
@@ -91,11 +141,11 @@ class CAPMBot(Agent):
 
         # Set stocks
         for market, asset in holdings.assets.items():
-            self._current_stocks[market.name] = asset.units_available
+            self._current_stocks[market.item] = asset.units_available
 
         print(f"I have holding cash {holdings.cash} and cash available {holdings.cash_available}")
         for market, asset in holdings.assets.items():
-            print(f"Assets settled {asset.units} and available {asset.units_available} for market {market.name}")
+            print(f"Assets settled {asset.units} and available {asset.units_available} for market {market.item}")
 
 
 if __name__ == "__main__":
--- work2.py
+++ work2.py
@@ -4,3 +4,17 @@ print("Hello world!")
 
 # Use algovenv/Scripts/activate to activate the virtual environment in the algotrading folder
 # And `deactivate` to deactivate it
+
+# trail script
+from itertools import combinations
+
+for i in combinations(['StockA', 'StockB', 'StockC', 'Note'], 2):
+    print(i)
+
+#print(np.cov(np.dot([10, 0, 7.5, 2.5], [0,2.5,7.5,10])))
+print(np.average([10, 0, 7.5, 2.5]))
+print(np.dot([10, 0, 7.5, 2.5], [0,2.5,7.5,10]))
+
+a = np.array([10, 5, 32, 3]) / 5
+
+print(a)
